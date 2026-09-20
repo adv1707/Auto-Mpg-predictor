0b3c970cf4086b93236e74bb4d03a3353af8201b
ADITYA KUMAR VERMA <adityav1509@gmail.com>
2026-09-20 19:06:10 +0530

    Add files via upload

---
+++ CLI.py
@@ -0,0 +1,50 @@
+class frontend:
+    def __init__(self):
+        import Main as p 
+        import numpy as np
+        self.np = np
+        self.model = p
+        self.listnames = ['cylinders','displacement','horsepower','weight','acceleration','model_year','origin']
+        self.asker()
+    def asker(self):
+        while True:
+            try:
+                x = int(input('''What do you want to do :-
+                                 1)Enter 1 for making the prediction 
+                                 2)Enter 2 for Getting r2 score of our model
+                                 3)Enter any other number for exiting \n'''))
+            except Exception as e:
+                print("Please enter a suitable number!!!")
+                print("\n")
+                self.asker()
+            
+            if(x!=1 and x!=2):
+                print("Have a nice day!!")
+                exit()
+            elif(x==2):
+                self.r2()
+            else:
+                self.predictor()
+    
+    def r2(self):
+        print(self.model.r2)
+        print("\n")
+    
+    def predictor(self):
+        inputs = []
+        for i in self.listnames:
+            try:
+                y = float(input(f"Enter the value for {i} of your vehicle : "))
+            except Exception as e:
+                print("You didnt enter a valid value!! Try again")
+                print("\n")
+                self.predictor()
+            inputs.append(y)
+        inputarray = self.np.array(inputs).reshape(1,len(inputs))
+        print("\n")
+        print(f"The approximated value of Miles-per-Gallon according to your data is {self.model.mainpipe.predict(inputarray)}")
+        print("\n")
+        
+        
+        
+model = frontend()
---
+++ Main.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import SGDRegressor
+from sklearn.metrics import r2_score
+
+
+#Importing data
+data = pd.read_csv(r"auto-mpg.csv")
+
+#Filling missing values
+imputer = SimpleImputer(strategy="mean")
+y = imputer.fit_transform(data["horsepower"].reset_index().set_index("index")).ravel()
+y = pd.Series(y)
+data["horsepower"]=y
+
+#Dropping irrelevant column
+data=data.drop(columns=["car_name"])
+
+#Creating training and test data sets with mpg as target
+X=data.drop(columns=["mpg"])
+Y = data["mpg"]
+x_train , x_test , y_train ,y_test = train_test_split(X,Y,test_size=0.2,shuffle=True,random_state=17)
+
+#Creating functions 
+logfunc = FunctionTransformer(func=np.log)
+sqrtfunc = FunctionTransformer(func=np.sqrt)
+scaler = StandardScaler()
+
+#Creating log transformer
+logtransformer = ColumnTransformer(transformers=[
+    ("logtransformers for other columns",logfunc,[1,2,3])
+],remainder="passthrough")
+
+onehottransformer  = ColumnTransformer(transformers=[
+    ("Onehotencoding",OneHotEncoder(drop="first"),[6])
+],remainder="passthrough")
+
+
+#Creating scaler transformer
+scalertransformer = ColumnTransformer(transformers=[
+    ("scaler",scaler,[0,1,2,3,4,5])
+])
+
+#Creating side pipeline for cylinder transformer
+transformer1 = ColumnTransformer(transformers=[
+    ("sqrt",sqrtfunc,[0]),
+],remainder="passthrough")
+
+transformer2 = ColumnTransformer(transformers=[
+    ("log",logfunc,[0]),
+],remainder="passthrough")
+
+sidepipecylinder = Pipeline([
+    ("cylinder transformer1",transformer1),
+    ("cylinder transformer2",transformer2)
+])
+
+
+#Creating polymomial features
+polynomial = PolynomialFeatures(degree=(1,2))
+
+#Creating model
+sgd = SGDRegressor()
+
+
+#Creating mainpipeline 
+mainpipe = Pipeline([
+    ("cylinderpipeline",sidepipecylinder),
+    ("logtransformer",logtransformer),
+    ("onehottransformer",onehottransformer),
+    ("scaling",scalertransformer),
+    ("Raising power",polynomial),
+    ("model",sgd)
+])
+
+mainpipe.fit(x_train,y_train)
+y_pred = mainpipe.predict(x_test)
+r2 = r2_score(y_test,y_pred)
